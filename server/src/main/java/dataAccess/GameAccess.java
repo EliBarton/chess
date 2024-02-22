6ed0adf675e385773129114d61e05108adaf7aeb
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-02-22 14:42:05 -0700

    Added gameAccess

--- server/src/main/java/dataAccess/GameAccess.java
+++ server/src/main/java/dataAccess/GameAccess.java
@@ -1,4 +1,16 @@
 package dataAccess;
 
+import model.GameData;
+
+import java.util.HashSet;
+
 public interface GameAccess {
+
+    void createGame(String gameName, String authToken);
+
+    GameData getGame(String id);
+
+    HashSet<GameData> listGames();
+
+    void updateGame(String id, String gameString);
 }
