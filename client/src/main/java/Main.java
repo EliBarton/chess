841f0998568f1ac93b5d7b687fe4356e842969bf
EliBarton <70331034+EliBarton@users.noreply.github.com>
2024-03-14 13:59:58 -0600

    Started start menu and login

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,30 @@
 import chess.*;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner reader;
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        startMenu();
+
+    }
+
+    public static void startMenu(){
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Quit");
+        int input = reader.nextInt();
+
+        switch (input){
+            case 1 -> loginPrompt();
+        }
+    }
+
+    private static void loginPrompt(){
+        System.out.println("Enter your username: ");
+        String username = reader.nextLine();
+
     }
 }
